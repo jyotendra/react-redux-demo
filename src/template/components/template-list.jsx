0dbeaa441bf7ab3ae3b5c95d9a26db1ee3611aa3
jyotendra <jyotendras@neuronsolutions.com>
2018-08-29 15:59:22 +0530

    added key to template

--- src/template/components/template-list.jsx
+++ src/template/components/template-list.jsx
@@ -3,8 +3,8 @@ export const TemplateList = ({ templateList = [] }) => {
   return (
     <div>
       <ul>
-        {templateList.map(template => {
-          return <li>{template.name}</li>;
+        {templateList.map((template, index) => {
+          return <li key={index}>{template.name}</li>;
         })}
       </ul>
     </div>
